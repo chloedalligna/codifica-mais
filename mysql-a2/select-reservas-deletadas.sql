8e3f72a095849f6566837e5643dba10ddb2cef29
Chloe <raionbyjinn@gmail.com>
2025-10-28 21:01:43 -0300

    Novo folder mysql-a2/ e scripts do exercício mysql-a2 (NÃO FINALIZADO)

---
+++ mysql-a2/select-reservas-deletadas.sql
@@ -0,0 +1,3 @@
+# Arquivo select-reservas-deletadas.sql:
+# Liste todas as reservas marcadas como deletadas, com o nome do hóspede, nome do 
+# anfitrião, título da hospedagem e data de exclusão.
---
+++ mysql-a2/soma-valor-total-recebido.sql
@@ -0,0 +1,3 @@
+# Arquivo soma-valor-total-recebido.sql:
+# Mostre o valor total recebido com base nas reservas não deletadas, somando apenas o 
+# valor da diária multiplicado pelo número de noites.
