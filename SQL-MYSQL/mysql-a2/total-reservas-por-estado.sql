5c968471cc5841f1ccae04aae24528cefb1faf52
Chloe <raionbyjinn@gmail.com>
2025-10-29 07:38:57 -0300

    Merge do main nesta branch, movi o folder mysql-a2/ para o folder SQL-MYSQL/ e iniciei um script dos exercícios extras

---
+++ SQL-MYSQL/mysql-a2/total-reservas-por-estado.sql
@@ -0,0 +1,9 @@
+/* Arquivo total-reservas-por-estado.sql
+Liste o nome do estado, a quantidade total de reservas realizadas em hospedagens
+localizadas em cada estado e a soma total simples do valor das reservas (valor da diária
+multiplicado pelo número de noites).
+Considere apenas as reservas não deletadas e que tenham data de check-in anteriores a
+ou iguais Junho de 2025.
+Ordene o resultado de forma que os estados com maior número de reservas apareçam
+primeiro e, em caso de empate, o estado com maior valor total venha antes. */
+
